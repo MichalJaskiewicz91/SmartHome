0e1580e971fdb5008bd051e917dae2ee85478139
Michał <michalyjp@gmail.com>
2020-06-03 16:09:57 +0200

    First commit

---
+++ SmartHome/Models/MockSectionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHome.Models
+{
+    public class MockSectionRepository: IDeviceRepository
+    {
+        public IEnumerable<Section> AllSections =>
+            new List<Section>
+            {
+                new Section{SecctionId=1, SectionName="Electricity",Description="Lighting"}
+            };
+    }
+}
